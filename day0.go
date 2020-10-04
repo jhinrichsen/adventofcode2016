e9d43ea45dbbbdd1e304ce33670f4a71e0b1c08d
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2020-10-04 02:19:14 +0200

    refactor: rename day 00 to day 0

---
+++ day0.go
@@ -0,0 +1,11 @@
+package adventofcode2016
+
+// Day00Puzzle1 solves day 1 part 1.
+func Day00Puzzle1(lines []string) uint {
+	return 0
+}
+
+// Day00Puzzle2 solves day 1 part 1.
+func Day00Puzzle2(lines []string) uint {
+	return 0
+}
--- day0_test.go
+++ day0_test.go
@@ -12,12 +12,12 @@ var day00Examples = []struct {
 	{[]string{}, 0},
 }
 
-func TestDay00ExamplesPart1(t *testing.T) {
+func TestDay00ExamplesPuzzle1(t *testing.T) {
 	for _, tt := range day00Examples {
 		id := fmt.Sprintf("%s", tt.in)
 		t.Run(id, func(t *testing.T) {
 			want := tt.out
-			got := Day00Part1(tt.in)
+			got := Day00Puzzle1(tt.in)
 			if want != got {
 				t.Fatalf("want %d but got %d", want, got)
 			}
@@ -25,17 +25,17 @@ func TestDay00ExamplesPart1(t *testing.T) {
 	}
 }
 
-func TestDay00Part1(t *testing.T) {
+func TestDay00Puzzle1(t *testing.T) {
 	const want = 0
 	lines, err := linesFromFilename(filename(00))
 	if err != nil {
 		t.Fatal(err)
 	}
-	got := Day00Part1(lines)
+	got := Day00Puzzle1(lines)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay00Part2(t *testing.T) {
+func TestDay00Puzze2(t *testing.T) {
 }

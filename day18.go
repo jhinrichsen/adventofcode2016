60c9ffa8d6c3cacdcee1c53838edc61b6d472e01
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2023-10-22 17:08:16 +0200

    refactor: performance tuning

--- day18.go
+++ day18.go
@@ -9,11 +9,12 @@ const (
 	trap = 94 // '^'
 )
 
+// derive uses ancestors to produce a new safe or trap.
 // Its left and center tiles are traps, but its right tile is not.
 // Its center and right tiles are traps, but its left tile is not.
 // Only its left tile is a trap.
 // Only its right tile is a trap.
-func isTrap(left, center, right bool) bool {
+func derive(left, right bool) bool {
 	/* my working version
 	b := (left && center && (!right)) ||
 		(center && right && (!left)) ||
@@ -21,7 +22,7 @@ func isTrap(left, center, right bool) bool {
 		(right && (!left) && (!center))
 	*/
 
-	/* running the beast through an boolean simplifier
+	/* running the beast through a boolean algebra simplifier
 	(a && b && (!c)) ||
 	(b && c && (!a)) ||
 	(a && (!b) && (!c)) ||
@@ -30,7 +31,12 @@ func isTrap(left, center, right bool) bool {
 	=> (a && ~ c) || (~ a && c)
 	*/
 
-	return (left && (!right)) || ((!left) && right)
+	// after a closer inspection, the expression
+	// (left && (!right)) || ((!left) && right)
+	// is a plain XOR
+
+	// XOR the Go way:
+	return left != right
 }
 
 func next(row string) string {
@@ -49,8 +55,7 @@ func next(row string) string {
 		} else {
 			right = from[i+1] == trap
 		}
-		center := from[i] == trap
-		b := isTrap(left, center, right)
+		b := derive(left, right)
 		var c byte = safe
 		if b {
 			c = trap
@@ -61,10 +66,76 @@ func next(row string) string {
 }
 
 func Day18(row string, count int) int {
-	var n int
-	for i := count; i > 0; i-- {
-		n += strings.Count(row, string(rune(safe)))
+	return Day18V1(row, count)
+}
+
+func Day18V1(row string, count int) int {
+	var sum int
+	// for i := count; i > 0; i-- {
+	for i := 0; i < count; i++ {
+		n := strings.Count(row, string(rune(safe)))
+		sum += n
 		row = next(row)
 	}
+	return sum
+}
+
+// safesAndTraps are bit representations of '.' and '^'.
+// Bits are left and right padded with safe.
+type safesAndTraps []bool
+
+func (bits safesAndTraps) String() string {
+	var sb strings.Builder
+	for i := 1; i < len(bits)-1; i++ {
+		var c byte
+		if bits[i] {
+			c = trap
+		} else {
+			c = safe
+		}
+		sb.WriteByte(c)
+	}
+	return sb.String()
+}
+
+func safes(a safesAndTraps) int {
+	var n int
+	for i := len(a) - 2; i > 0; i-- {
+		if !a[i] {
+			n++
+		}
+	}
 	return n
+}
+
+func newSafesAndTraps(s string) safesAndTraps {
+	bits := make([]bool, 1+len(s)+1) // left and right padding
+
+	// we have two indices, loop over string index
+	for si := len(s) - 1; si >= 0; si-- {
+		bits[1+si] = s[si] == trap
+	}
+	return bits
+}
+
+func step(pred safesAndTraps, succ safesAndTraps) {
+	// not sure if constants are lifted out of the loop
+	// https://github.com/golang/go/issues/15808
+	for i := len(pred) - 2; i > 0; i-- {
+		succ[i] = derive(pred[i-1], pred[i+1])
+	}
+}
+
+func Day18V2(row string, count int) int {
+	a := newSafesAndTraps(row)
+	b := make(safesAndTraps, len(a))
+
+	var sum int
+	for i := count; i > 0; i-- {
+		sum += safes(a)
+		step(a, b)
+		a, b = b, a
+	}
+
+	return sum
 }
--- day18_test.go
+++ day18_test.go
@@ -1,6 +1,28 @@
 package adventofcode2016
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+const (
+	input  = "^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^"
+	repeat = 400000
+	want   = 19991126
+)
+
+var day18Example = []string{
+	".^^.^.^^^^",
+	"^^^...^..^",
+	"^.^^.^.^^.",
+	"..^^...^^^",
+	".^^^^.^^.^",
+	"^^..^.^^..",
+	"^^^^..^^^.",
+	"^..^^^^.^^",
+	".^^^..^.^^",
+	"^^.^^^..^^",
+}
 
 func TestDay18Example(t *testing.T) {
 	const (
@@ -13,7 +35,99 @@ func TestDay18Example(t *testing.T) {
 	}
 }
 
-func TestDay18LargeExample(t *testing.T) {
+func TestDay18NewSafesAndTraps(t *testing.T) {
+	s := "^.^.^.^^^^"
+	want := safesAndTraps{
+		false, // padding
+		true, false, true, false, true, false, true, true, true, true,
+		false} // padding
+
+	got := newSafesAndTraps(s)
+	if len(want) != len(got) {
+		t.Fatalf("want len %d but got %d", len(want), len(got))
+	}
+	if !reflect.DeepEqual(want, got) {
+		t.Fatalf("want %+v but got %+v", []bool(want), []bool(got))
+	}
+
+}
+
+func TestDay18String(t *testing.T) {
+	bits := safesAndTraps{
+		false, // padding
+		false, true, true, false, true, false, true, true, true, true,
+		false} // padding
+	want := ".^^.^.^^^^"
+	got := bits.String()
+	if len(want) != len(got) {
+		t.Fatalf("want len %d but got %d", len(want), len(got))
+	}
+	if want != got {
+		t.Fatalf("want %+v but got %+v", want, got)
+	}
+
+}
+
+// TestDay18Conversion tests one string -> bits -> string transformation to
+// make sure the safe padding is correct.
+func TestDay18Conversion(t *testing.T) {
+	wants := []string{
+		".^^.^.^^^^",
+		input,
+	}
+	for _, want := range wants {
+		got := newSafesAndTraps(want).String()
+		if len(want) != len(got) {
+			t.Fatalf("want len %d but got %d", len(want), len(got))
+		}
+		if want != got {
+			t.Fatalf("want %q but got %q", want, got)
+		}
+	}
+}
+
+func TestDay18ExampleV1(t *testing.T) {
+	const want = 38
+	for i := 1; i < len(day18Example); i++ {
+		want := day18Example[i]
+		got := next(day18Example[i-1])
+		if want != got {
+			t.Fatalf("want %q but got %q", want, got)
+		}
+	}
+
+	got := Day18V1(day18Example[0], len(day18Example))
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay18Part1(t *testing.T) {
+	const want = 1974
+	got := Day18(input, 40)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func TestDay18Part2(t *testing.T) {
+	TestDay18Part2V1(t)
+}
+
+func TestDay18Part2V1(t *testing.T) {
+	got := Day18(input, repeat)
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func BenchmarkDay18Part2V1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Day18V1(input, repeat)
+	}
+}
+
+func TestDay18ExampleV2(t *testing.T) {
 	const want = 38
 	var lines = []string{
 		".^^.^.^^^^",
@@ -30,45 +144,33 @@ func TestDay18LargeExample(t *testing.T) {
 
 	for i := 1; i < len(lines); i++ {
 		want := lines[i]
-		got := next(lines[i-1])
+		from := newSafesAndTraps(lines[i-1])
+		into := newSafesAndTraps(lines[i])
+		step(from, into)
+		got := into.String()
 		if want != got {
-			t.Fatalf("want %q but got %q", want, got)
+			t.Fatalf("line %d:  want %q but got %q", i, want, got)
 		}
 	}
 
-	got := Day18(lines[0], len(lines))
+	got := Day18V2(lines[0], len(lines))
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay18Part1(t *testing.T) {
-	const (
-		input = "^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^"
-		want  = 1974
-	)
-	got := Day18(input, 40)
+func TestDay18Part2V2(t *testing.T) {
+	got := Day18V2(input, repeat)
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-func TestDay18Part2(t *testing.T) {
-	const (
-		input = "^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^"
-		want  = 19991126
-	)
-	got := Day18(input, 400000)
-	if want != got {
-		t.Fatalf("want %d but got %d", want, got)
-	}
-}
-
-func BenchmarkDay18Part2(b *testing.B) {
+func BenchmarkDay18Part2V2(b *testing.B) {
 	const (
 		input = "^.....^.^^^^^.^..^^.^.......^^..^^^..^^^^..^.^^.^.^....^^...^^.^^.^...^^.^^^^..^^.....^.^...^.^.^^.^"
 	)
 	for i := 0; i < b.N; i++ {
-		_ = Day18(input, 400000)
+		_ = Day18V2(input, repeat)
 	}
 }

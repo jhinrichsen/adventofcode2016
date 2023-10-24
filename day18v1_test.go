b126e5ba0dbf63741fdbc061c384274d1aacf9d3
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2023-10-24 21:28:25 +0200

    test(day18): complete tests and benches

--- day18v1_test.go
+++ day18v1_test.go
@@ -17,29 +17,29 @@ func TestDay18Example(t *testing.T) {
 
 func TestDay18ExampleV1(t *testing.T) {
 	const want = 38
-	for i := 1; i < len(day18Example); i++ {
-		want := day18Example[i]
-		got := next(day18Example[i-1])
+	for i := 1; i < len(day18ExampleInput); i++ {
+		want := day18ExampleInput[i]
+		got := next(day18ExampleInput[i-1])
 		if want != got {
 			t.Fatalf("want %q but got %q", want, got)
 		}
 	}
 
-	got := Day18V1(day18Example[0], len(day18Example))
+	got := Day18V1(day18ExampleInput[0], len(day18ExampleInput))
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
 func TestDay18Part2V1(t *testing.T) {
-	got := Day18(input, repeat)
-	if want != got {
-		t.Fatalf("want %d but got %d", want, got)
+	got := Day18(day18Input, day18RepeatPart2)
+	if day18WantPart2 != got {
+		t.Fatalf("want %d but got %d", day18WantPart2, got)
 	}
 }
 
 func BenchmarkDay18Part2V1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Day18V1(input, repeat)
+		_ = Day18V1(day18Input, day18RepeatPart2)
 	}
 }
--- day18v3_test.go
+++ day18v3_test.go
@@ -33,23 +33,34 @@ func TestTrapsAsString(t *testing.T) {
 
 func TestDay18ExampleV3(t *testing.T) {
 	const want = 38
-	got := Day18V3(day18Example[0], len(day18Example))
+	got := Day18V3(day18ExampleInput[0], len(day18ExampleInput))
 	if want != got {
 		t.Fatalf("want %d but got %d", want, got)
 	}
 }
 
-// TODO add Part1V3
+func TestDay18Part1V3(t *testing.T) {
+	got := Day18V3(day18Input, day18RepeatPart1)
+	if day18WantPart1 != got {
+		t.Fatalf("want %d but got %d", day18WantPart1, got)
+	}
+}
+
+func BenchmarkDay18Part1V3(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = Day18V3(day18Input, day18RepeatPart1)
+	}
+}
 
 func TestDay18Part2V3(t *testing.T) {
-	got := Day18V3(input, repeat)
-	if want != got {
-		t.Fatalf("want %d but got %d", want, got)
+	got := Day18V3(day18Input, day18RepeatPart2)
+	if day18WantPart2 != got {
+		t.Fatalf("want %d but got %d", day18WantPart2, got)
 	}
 }
 
 func BenchmarkDay18Part2V3(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_ = Day18V3(input, repeat)
+		_ = Day18V3(day18Input, day18RepeatPart2)
 	}
 }

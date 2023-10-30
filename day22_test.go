234b0bb132252b05f3eda1d53efa48a1a9e896f8
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2023-10-30 19:24:46 +0100

    feat: split into part 1 and 2

--- day22_test.go
+++ day22_test.go
@@ -2,7 +2,7 @@ package adventofcode2016
 
 import "testing"
 
-func TestDay22(t *testing.T) {
+func TestDay22Part1(t *testing.T) {
 	const (
 		part1 = true
 		// want  = 20636 // first try wrong, too high
@@ -23,9 +23,44 @@ func TestDay22(t *testing.T) {
 	}
 }
 
-func BenchmarkDay22(b *testing.B) {
+func TestDay22Part2(t *testing.T) {
+	const (
+		part1 = false
+		// want  = 20636 // first try wrong, too high
+		// i was using col 1 (size) and col 2 (used) instead of col 2 (used) and col 3 (avail)
+		// does that count as 'off-by-one'? not sure...
+		want = 0
+	)
+	lines, err := linesFromFilename(filename(22))
+	if err != nil {
+		t.Fatal(err)
+	}
+	got, err := Day22(lines, part1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want != got {
+		t.Fatalf("want %d but got %d", want, got)
+	}
+}
+
+func BenchmarkDay22Part1(b *testing.B) {
 	const (
 		part1 = true
+	)
+	lines, err := linesFromFilename(filename(22))
+	if err != nil {
+		b.Fatal(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = Day22(lines, part1)
+	}
+}
+
+func BenchmarkDay22Part2(b *testing.B) {
+	const (
+		part1 = false
 	)
 	lines, err := linesFromFilename(filename(22))
 	if err != nil {
